4844b4b0490d54d9916c73aec8d8ee4b9575656a
Michael Cook <michael.cook@techdept.co.uk>
2017-11-29 17:36:14 +0000

    Doesn't work, think I need to split it more as a service/module

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
+// Tome of the Unknown!
+// Based on:
+// https://medium.com/@cyrilletuzi/how-to-build-and-publish-an-angular-module-7ad19c0b4464
+//
+
 export { TomeOfTheUnknown } from './src/tome.module';
--- src/tome.module.ts
+++ src/tome.module.ts
@@ -1,11 +1,30 @@
 import { NgModule } from '@angular/core';
+import { HttpModule, Http, Response } from '@angular/http';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule, HttpModule]
 })
 export class TomeOfTheUnknown {
-  constructor() {
+  articles: any;
+
+  constructor(private http: Http) {
     console.log('HELLO FROM TOME');
+    console.log('Attempting to read a JSON file..');
+    this.testRead().then(results => {
+      console.log(results);
+    });
+  }
+
+  // This should be in a .service
+  testRead() {
+    return new Promise(resolve => {
+      this.http.get('/assets/articles.json').subscribe(response => {
+        // console.log('WE ONLY WANT TO RUN THIS ONCE');
+        this.articles = response.json();
+        console.log(response);
+        resolve(this.articles);
+      });
+    });
   }
 }
